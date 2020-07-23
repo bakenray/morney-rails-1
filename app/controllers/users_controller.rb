2fc1f8ee2f8fe0102301eb91fb564527125a8dd1
bakenray <lpqstrong@163.com>
2020-07-23 15:53:53 +0800

    数据校验 邮箱和密码

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
   def create
+
     user = User.new
     user.email = params[:email]
     user.password = params[:password]
     user.password_confirmation = params[:password_confirmation]
-    user.save
-    p user.errors
+
+    if user.save
+      render json: {resource:user}, status: 200
+    else
+      render json: {errors:user.errors} ,status: 400
+    end
+
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
+
+  # 设置不能为空
+  validates_presence_of :email
+  validates_presence_of :password_confirmation, on: [create]
+
+  #  emails 判断必须有@
+  validates_format_of  :email, with: /.+@.+/
+  # 长度限制
+  validates :password,length: { minimum:6 }, on: [:create]
+  # 或者可以写成
+  # validates_length_of :password, minimum: 6
 end
+
