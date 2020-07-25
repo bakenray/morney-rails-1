65d07af5429be929d44eda3aae6d9e6465df90a1
bakenray <lpqstrong@163.com>
2020-07-25 19:04:40 +0800

    获取当前用户信息  、优化代码

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::API
   def render_resource (resource)
+    
+    return head 404 if resource.nil? #如果得到资源为空，就返回404
     if resource.errors.empty?
       render json: {resource:resource}, status: 200
     else
       render json: {errors:resource.errors} ,status: 400
     end
+  end
+  def current_user
+    @current_user ||= User.find_by_id session[:current_user_id]
   end
 end
