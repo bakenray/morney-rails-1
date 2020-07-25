9056a3692e9d5e00a4c40fb1ce57f16cbcb8522a
bakenray <lpqstrong@163.com>
2020-07-25 15:52:38 +0800

    自定义校验，邮箱是否注册

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,8 +2,16 @@ class Session
     include ActiveModel::Model
     attr_accessor :email, :password
     
-    validates :email, presence: true #email不能为空
+    validates :email, presence: true # email不能为空
+    validate :check_email, if: :email  #自定义校验，不带s 传有email，就校验email是否存在
     validates :password, presence: true #password不能为空
-    validates_format_of :email,with: /.+@.+/, if: :email  #验证邮箱
     validates :password, length: {minimum:6}, if: :password #密码长度限制
+    validates_format_of :email,with: /.+@.+/, if: :email  #验证邮箱格式
+
+    def check_email
+        user = User.find_by_email email  #也可以写成 User.find_by email: email
+        if user.nil? # 判断user为空
+            errors.add :email, :not_found
+        end
+    end
 end
